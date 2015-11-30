c77ea52ed0e1ae9dc5d8ad2bf10f0aaf1c917cae
Corey Finley <finley.corey@gmail.com>
2015-11-30 11:21:14 -0500

    updates picture

---
+++ script.js
@@ -0,0 +1,8 @@
+var replacements = new Object({});
+replacements.developer = "Ruby on Rails, MongoDB, and ElasticSearch developer";
+
+$(document).ready(function(){
+  $("button").click(function(){
+      $("p").hide();
+  });
+});
