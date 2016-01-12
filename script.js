bd33590b542d2dbf26bd1adee27e63d69aae6c37
Corey Finley <finley.corey@gmail.com>
2016-01-11 20:53:55 -0500

    adds jekyll blogging

--- script.js
+++
@@ -1,8 +0,0 @@
-var replacements = new Object({});
-replacements.developer = "Ruby on Rails, MongoDB, and ElasticSearch developer";
-
-$(document).ready(function(){
-  $("button").click(function(){
-      $("p").hide();
-  });
-});
