2d20066435be129b4652c60c0c5106f7437a5f14
Corey Finley <finley.corey@gmail.com>
2019-02-15 14:38:35 -0500

    wip

--- javascripts/log.js
+++ javascripts/log.js
@@ -1,12 +1,29 @@
 $( document ).ready(function() {
+  idStr = (str) => {
+    return str.replace(/(\s|:|,)/g,"-");
+  }
+
   style="background-image: ;"
   mediaEl = "<media style=\""+style+"\"></media>"
   $("#header-image-container").css("background-image", "url('../media/bg2.jpeg')");
+  $nav = $("nav ul")
+  $.map($(".entry"), (el)=>{
+    $el = $(el);
+    title = $el.data("timestamp") + " - " + $el.data("title")
+    id = idStr($el.data("timestamp"))
+    console.log(id);
+    $el.attr("id", id);
+    $($el.find(".date")).text(title);
+    $nav.append(
+      "<li><a href=\"#"+id+"\">"+title+"</a></la>"
+    );
+    // debugger
+  });
   $.map($(".inline-image"), (el)=>{
     $el = $(el);
-    console.log($el.data("img"));
-    $el.append(
-      "<img src=\"../"+$el.data("img")+"\"/ width=\"100%\">"
-    );
+    src = $el.data("img").replace("www.dropbox.com", "dl.dropboxusercontent.com");
+    $img = $($el.find("img"));
+    $img.attr("src", src);
+    $img.removeClass("placeholder");
   });
 });
