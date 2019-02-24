3982defaadbd34cc6dfea3c08b0391c65382a335
Corey Finley <finley.corey@gmail.com>
2019-02-24 15:56:26 -0500

    react test

--- javascripts/site.js
+++
@@ -1,5 +0,0 @@
-$( document ).ready(function() {
-  style="background-image: ;"
-  mediaEl = "<media style=\""+style+"\"></media>"
-  $("#header-image-container").css("background-image", "url('media/bg2.jpeg')")
-});
